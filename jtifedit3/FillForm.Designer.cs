a74c732dc5bcc7456797e669c42d875ede51e642
KU <ku@digitaldolphins.jp>
2017-04-24 14:27:56 +0900

    塗りつぶし画面の表示不具合修正。

--- jtifedit3/FillForm.Designer.cs
+++ jtifedit3/FillForm.Designer.cs
@@ -123,8 +123,8 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(889, 578);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Name = "FillForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
